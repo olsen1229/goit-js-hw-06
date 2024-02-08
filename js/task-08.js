ed1a06dcbb0b72dc047a3a33ba125bc34c8af673
olsen1229 <marylou.olsen7@gmail.com>
2024-02-08 23:33:44 +0800

    task update

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+function processLoginForm() {
+  const form = document.getElementById("loginForm");
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
 
+  if (!email || !password) {
+    alert("All fields must be filled in.");
+    return;
+    }
+    const formData = {
+    email: email,
+    password: password,
+    };
+    
+    form.reset();
+}
+
+  
+  
+  
+  
